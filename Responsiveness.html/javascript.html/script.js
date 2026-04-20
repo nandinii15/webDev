4f30bed091c7a828c6882b493c55a1c23fb75f85
nandinii15 <nandinishukla0501@gmail.com>
2026-04-20 20:41:15 +0530

    script.html

---
+++ Responsiveness.html/javascript.html/script.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+console.log("hello world 121123");
+
+
+let a=5;
+
+var x=10;
+var x=20;
+
+const pi =3.14;
+pi=41;
+
+console.log("task complete")
+
+let x=10;
+let y="10";
+
+
+console.log(x==y);
+
+console.log(x===y);
