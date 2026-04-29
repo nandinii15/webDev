718d0a8b206a644df88830b2e4e8dc3483ea0ab6
nandinii15 <nandinishukla0501@gmail.com>
2026-04-29 19:13:30 +0530

    message.html

--- Responsiveness.html/javascript.html/script.js
+++ Responsiveness.html/javascript.html/script.js
@@ -18,10 +18,6 @@ let x=10;
 
 
 
-
-
-
-
 let y="10";
 
 console.log(x==y);
---
+++ Responsiveness.html/js.html/script.js
@@ -0,0 +1,18 @@
+for(var i=1;i<=10;i++)
+{
+    console.log(i*2);
+}
+
+let a=[24, 58, 33,86,96,12,53,23,23,32,100]
+
+     console.log("array elements");
+     for(var i=0; i<10; i++)
+
+{
+    console.log(a[i]);
+}
+
+let i=0;
+while (i<10); {
+console.log(a[i ])
+}
