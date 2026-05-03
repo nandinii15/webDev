2ce6138ca1d34536bbe975fac265cfa38e41a8f1
nandinii15 <nandinishukla0501@gmail.com>
2026-05-03 20:54:27 +0530

    message.html

--- jsCondition.html/javascript/script.js
+++ jsCondition.html/javascript/script.js
@@ -1,1 +1,61 @@
-let =
+// var a =10;
+// console.log(a);
+// console.log(b);
+// console.log();{
+
+
+
+let test= "this is a global function"
+
+function classA(){
+   console.log("ClassA",test);
+}
+function classB(){
+    console.log("ClassB",test);
+    
+}
+function classC(){
+    let test2= "this is local variable";
+    console.log( "ClassC",test);
+    console.log("Class", test2)
+}
+function classD(){
+    let x= 20;
+    console.log( "ClassD",test);
+    console,log(x);
+}
+let v=10;
+console.log(v);
+console.log(x);
+{
+    console.log(v);
+    console.log(x);
+
+}
+console.log(x);
+
+function classE(){
+    console.log("ClassE",test);
+}
+let a =5;
+function classF(){
+    console.log( "ClassF" ,test);
+    console.log(a);
+}
+function classG(){
+    console.log("ClassG",test);
+    console.log(a);
+}
+function classB(){
+    console.log("ClassB",test);
+    console.log(a);
+}
+
+
+classA();
+classB();
+classC();
+classD();
+classE();
+classF();
+classG();
---
+++ jsCondition.html/javascript/script2.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+let a;
+let b;
+let c;
+
+a=10;
+console.log(a);
+console.log(b);
+console.log(c);
+
+b=15;
+console.log(a);
+console.log(b);
+console.log(c);
+
+c=20;
+console.log(a);
+console.log(b);
+console.log(c);
+
+abc();
+function abc(){
+    console.log("i am a function")
+}
+function submit(){
+    console.log("submit button clicked")
+}
+
