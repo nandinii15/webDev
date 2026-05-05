9887cbbfdc8a94cbbf0bf6d7d150253d264363e9
nandinii15 <nandinishukla0501@gmail.com>
2026-05-05 19:45:26 +0530

    message.html

---
+++ Responsiveness.html/jsdom/script2.js
@@ -0,0 +1,39 @@
+let arr=[12,34,56,34,45,67,78];
+for (var i=0;i<7;i++);
+{
+    console.log(arr[i])
+}
+ console.log(arr);
+
+ arr[2] = arr[2] +10;
+ console.log(arr);
+ arr=[];
+  
+ console.log(arr);
+
+ arr.pop();
+
+ console.log(arr);
+
+ arr.push(99);
+
+ console.log(arr);
+
+ arr.unshift(11);
+
+ console.log(arr);
+
+ arr.shift();
+
+ console.log(arr);
+
+ console.log(arr.indexOf(78));
+
+ console.log(arr.includes(34));
+
+ let unar = [56,23,89,45,67,34,12,90,87,45];
+ console.log(unar);
+
+ unar.sort();
+
+ console.log
