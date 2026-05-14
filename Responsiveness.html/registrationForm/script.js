f49050256399821200372db0c33b7366b83ef1eb
nandinii15 <nandinishukla0501@gmail.com>
2026-05-14 18:35:25 +0530

    message.html

---
+++ Responsiveness.html/registrationForm/script.js
@@ -0,0 +1,7 @@
+// document.getElementById("registrationForm").addEventListener("submit",(event)=>{
+   
+//     event.preventDefault();
+// } 
+ const fn = document.getElementById("FullName").value;
+ const gender = document.querySelector("input[name="gender"]:checked")?.value;
+ 
