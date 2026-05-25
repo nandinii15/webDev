fff066697958f84c2a80204a3d13d0acd15caa6e
nandinii15 <nandinishukla0501@gmail.com>
2026-05-25 19:43:44 +0530

    message.html

--- weatherApi/script.js
+++ weatherApi/script.js
@@ -1,34 +1,41 @@
-const API_KEY = "Your-Open-Weather-API-Key";
+const API_KEY = "f68cced823131fd27b6a3e53c987e0f6";
 
 async function getWeather() {
-  const cityName = document.getElementById("cityName").value.trim();
 
-  const { Lattitude, Longitude } = await getGeoLocation(cityName);
+    const cityName = document.getElementById("cityName").value.trim();
 
-  //   console.log({ Lattitude, Longitude });
+    const { latitude, longitude } = await getGeoLocation(cityName);
 
-  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+    // Weather API
+    const WEATHER_API =
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
-  const response = await fetch(WEATHER_API);
-  const data = await response.json();
+    const response = await fetch(WEATHER_API);
 
-  //console.log(data);
+    const data = await response.json();
 
-  const temperature = data.main.temp - 273.15;
+    console.log(data);
 
-  document.getElementById("Temperature").innerText = temperature.toFixed(2);
+    // Convert Kelvin to Celsius
+    const temperature = data.main.temp - 273.15;
+
+    document.getElementById("temp").innerText =
+        temperature.toFixed(2) + " °C";
 }
 
 async function getGeoLocation(city) {
-  const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-  const response = await fetch(GEO_LOC_API);
-  const data = await response.json();
+    const GEO_LOC_API =
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-  //   console.log(data);
+    const response = await fetch(GEO_LOC_API);
 
-  const Lattitude = data[0].lat;
-  const Longitude = data[0].lon;
+    const data = await response.json();
 
-  return { Lattitude, Longitude };
+    console.log(data);
+
+    const latitude = data[0].lat;
+    const longitude = data[0].lon;
+
+    return { latitude, longitude };
 }
